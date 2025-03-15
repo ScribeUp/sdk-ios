452596df86d225bcaf60a16df03bb5920570c760
Yohei Oka <yoyo716jp@gmail.com>
2025-03-14 21:36:13 -0700

    add version 0.0.1

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.3
+ import PackageDescription
+
+ let package = Package(
+     name: "ScribeUpSDK",
+     products: [
+         .library(
+             name: "ScribeUpSDK",
+             targets: ["ScribeUpSDK"]
+         ),
+     ],
+     dependencies: [
+     ],
+     targets: [
+        .binaryTarget(
+          name: "ScribeUpSDK",
+          path: "ScribeUpSDK.xcframework"
+        ),
+     ]
+ )
